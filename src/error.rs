4249511fb083c4ce74d98ba5c39dad8fffc77a05
Jess <jess@ros.io>
2022-07-26 09:25:08 -0600

    Remove from impls and use thiserror macro

--- src/error.rs
+++ src/error.rs
@@ -6,31 +6,13 @@ use tokio::task::JoinError;
 #[derive(Error, Debug)]
 pub enum AJobQueueError {
     #[error("Execution error")]
-    Execution(#[source] ExecutionError),
+    Execution(#[from] ExecutionError),
 
     #[error("Storage error")]
-    Storage(#[source] StorageError),
+    Storage(#[from] StorageError),
 
     #[error("Job run error")]
-    JobRun(#[source] JobRunError),
-}
-
-impl From<ExecutionError> for AJobQueueError {
-    fn from(err: ExecutionError) -> Self {
-        AJobQueueError::Execution(err)
-    }
-}
-
-impl From<StorageError> for AJobQueueError {
-    fn from(err: StorageError) -> Self {
-        AJobQueueError::Storage(err)
-    }
-}
-
-impl From<JobRunError> for AJobQueueError {
-    fn from(err: JobRunError) -> Self {
-        AJobQueueError::JobRun(err)
-    }
+    JobRun(#[from] JobRunError),
 }
 
 #[derive(Error, Debug)]
@@ -39,7 +21,7 @@ pub enum ExecutionError {
     SignalingError(#[source] Box<dyn StdError + Send>),
 
     #[error("Failed to join executor tasks to main task")]
-    JoinError(#[source] JoinError),
+    JoinError(#[from] JoinError),
 }
 
 #[derive(Error, Debug)]
